b106cd0e3714a4eea87a518e4cff9d9138b61253
Amy Dang <kirontoo@gmail.com>
2021-10-02 20:24:41 -0700

    Methods to create & delete tasts, fetch all tasks

--- db/db.go
+++ db/db.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"encoding/binary"
 	"time"
 
 	"github.com/boltdb/bolt"
@@ -12,7 +13,7 @@ var db *bolt.DB
 var taskBucket = []byte("tasks")
 
 type Task struct {
-	ID    int
+	Key   int
 	Value string
 }
 
@@ -29,6 +30,73 @@ func Init(dbPath string) error {
 	})
 }
 
-func AddTask(task Task) error {
+func CreateTask(task string) (int, error) {
+	var id int
+	err := db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket(taskBucket)
+		id64, _ := b.NextSequence()
+		id = int(id64)
+		key := itob(id)
+		return b.Put(key, []byte(task))
+	})
+
+	if err != nil {
+		return -1, err
+	}
+
+	// return id, nil
+	return id, nil
+}
+
+// Integer to Byte
+func itob(v int) []byte {
+	b := make([]byte, 8)
+	binary.BigEndian.PutUint64(b, uint64(v))
+	return b
+}
+
+// Byte to Integer
+func btoi(b []byte) int {
+	return int(binary.BigEndian.Uint64(b))
+}
+
+func Close() {
+	db.Close()
+}
+
+func GetAllTasks() ([]Task, error) {
+	var tasks []Task
+	err := db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket(taskBucket)
+
+		c := b.Cursor()
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			// only check keys because they hsould never be nil
+			tasks = append(tasks, Task{
+				Key:   btoi(k),
+				Value: string(v),
+			})
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return tasks, nil
+}
+
+func DeleteTask(key int) error {
+	err := db.Update(func(tx *bolt.Tx) error {
+		id := itob(key)
+		return tx.Bucket(taskBucket).Delete(id)
+	})
+
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
