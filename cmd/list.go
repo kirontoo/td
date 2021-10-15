3e72987cc44534e708aa7b5849bccfa36b50e7b1
Amy Dang <kirontoo@gmail.com>
2021-10-14 19:10:09 -0700

    Clean up

--- cmd/list.go
+++ cmd/list.go
@@ -42,14 +42,6 @@ var all bool
 func init() {
 	rootCmd.AddCommand(listCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// listCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
 	listCmd.Flags().BoolVarP(&completed, "completed", "c", false, "List completed tasks")
 	listCmd.Flags().BoolVarP(&all, "all", "a", false, "List all tasks")
 }
